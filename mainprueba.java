d70d95e8f11929d3e39f02fb98c83150eedbb9b7
DavidLopez01 <dlcloc11@gmail.com>
2024-08-10 06:15:24 -0500

    Primer commit de prueba.

---
+++ mainprueba.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class mainprueba {
+    public static void main(String[] args) {
+        punto5 p5 = new punto5();
+        ArrayList<Integer> vector = p5.DevolverVec(5);
+        System.out.println(vector);
+
+    }
+}

db82efffaf372b88325404d76974cd0ff4ac029a
DavidLopez01 <dlcloc11@gmail.com>
2024-08-10 12:14:18 -0500

    Se agregan dos ejercicios nuevos resueltos

---
+++ ejercicio1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ejercicio1 {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el número de filas: ");
+        int a=input.nextInt();
+        System.out.println("Ingrese el número de columnas: ");
+        int b=input.nextInt();
+
+        int matriz [][] = new int[a][b];
+
+        for (int i=0; i<a; i++){
+            for (int j=0; j<b; j++){
+                System.out.println("ingrese el número de la celda "+i+" "+j);
+                matriz[i][j]=input.nextInt();
+            }
+
+        }
+        System.out.println( "La matriz es: ");
+        for (int i=0; i<a; i++){
+            for(int j=0; j<b; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Y su suma es: ");
+        int sum=0;
+        for (int i=0; i<a; i++){
+            for (int j=0; j<b; j++){
+                sum += matriz[i][j];
+            }
+        }
+        System.out.println(sum);
+        input.close();
+
+    }
+
+}
---
+++ ejercicio2.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+public class ejercicio2 {
+    public static void main(String[]args){
+
+        Random random=new Random();
+        List <Integer> numeros = new ArrayList<>();
+        for (int i =0;i<6*6;i++){
+            numeros.add(i);
+        }
+
+        Collections.shuffle(numeros, random);
+
+        
+        int matriz [][] = new int[6][6];
+        int index = 0;
+
+        for (int i=0; i<6; i++){
+            for (int j=0; j<6; j++){
+                matriz[i][j]=numeros.get(index++);
+            }
+        }
+
+        System.out.println( "La matriz es: ");
+        for (int i=0; i<6; i++){
+            for(int j=0; j<6; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Y el numero mayor de la matriz es: ");
+        int validador=0;
+        for (int i=0; i<6; i++){
+            for (int j=0; j<6; j++){
+                if (matriz[i][j]>validador){
+                    validador = matriz[i][j];
+                }
+            }
+        }
+        System.out.println(validador);
+    }
+}
